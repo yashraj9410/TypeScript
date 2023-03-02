d5122364ce2d7fdd70ddfd1783cd2561e690a687
Yash <yashraj7011@gmail.com>
2023-03-02 11:32:14 +0530

    generics

---
+++ practice/typescript/generics.js
@@ -0,0 +1,28 @@
+// generics are used to make reusable code 
+// generic locks the paramter type and return type of any function
+function func(val) {
+    return val;
+}
+function getuser(val) {
+    return val;
+}
+//here all parameters and return type id set to interface type user 
+// using array with generics 
+function getSearchElement(products) {
+    return products[3];
+    //we cannot directly return the index 3 because return type will also shift to element of array 
+    // that means if i am taking a parameter an array then return type T signifies that a value from the array must be returned 
+    // to return a number we must set return type of function from  T to number 
+}
+//creating a arrow function
+var getElement = function (products) {
+    return products[3];
+};
+var userDetails = function (val1, val2) {
+    return {
+        val1: val1,
+        val2: val2
+    };
+};
+console.log(userDetails("Not Permitted", { name: "Yash", id: 123 }));
+// generics classes 
--- practice/typescript/generics.ts
+++ practice/typescript/generics.ts
@@ -1,7 +1,7 @@
 // generics are used to make reusable code 
 // generic locks the paramter type and return type of any function
 
-function func<Type>(val:Type):Type{
+function func<Type>(val: Type): Type {
     return val;
 }
 
@@ -13,13 +13,51 @@ function func<Type>(val:Type):Type{
 
 // if we want to create our own type 
 
-interface User{
-    name:string;
-    age:number;
+interface User {
+    name: string;
+    age: number;
 }
 
-function getuser<User>(val:User):User{
+function getuser<User>(val: User): User {
     return val;
 }
 
 //here all parameters and return type id set to interface type user 
+
+// using array with generics 
+
+function getSearchElement<T>(products: T[]): T {
+    return products[3];
+    //we cannot directly return the index 3 because return type will also shift to element of array 
+    // that means if i am taking a parameter an array then return type T signifies that a value from the array must be returned 
+    // to return a number we must set return type of function from  T to number 
+
+}
+
+//creating a arrow function
+
+const getElement = <T>(products: T[]): T => {
+    return products[3];
+}
+
+//<T,> signifies that it is not an html or jsx tag mainly in react 
+
+// using extends in generics 
+// extends in generics may be used to signify type that may belong to another entity e.g interface
+
+interface userData {
+    name: string
+    id: number
+}
+
+const userDetails = <T, U extends userData>(val1: T, val2: U): object => {
+    return {
+        val1,
+        val2
+    }
+}
+
+console.log(userDetails("Not Permitted", { name: "Yash", id: 123 }))
+// generics classes 
+
+export { }

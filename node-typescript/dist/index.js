bb6fd93b8dc561c886a421407338f288d60d0f03
Yash <yashraj7011@gmail.com>
2023-03-21 18:46:34 +0530

    typescript express

---
+++ node-typescript/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+app.listen(2000, () => {
+    console.log(`Running @ 2000`);
+});
+// this is the ts file

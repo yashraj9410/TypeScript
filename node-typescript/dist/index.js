4aeb6ee2764569a64d1753b64b37bb9627216e79
Yash <yashraj7011@gmail.com>
2023-03-22 11:05:10 +0530

    express-typescript

--- node-typescript/dist/index.js
+++ node-typescript/dist/index.js
@@ -5,10 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+// get request to display Hello on page on port 2000
 app.get("/", (req, res) => {
     res.send("Hello");
 });
+// app listening at port 2000
 app.listen(2000, () => {
     console.log(`Running @ 2000`);
 });
-// this is the ts file
--- node-typescript/src/index.ts
+++ node-typescript/src/index.ts
@@ -2,10 +2,17 @@ import express from 'express';
 
 const app = express();
 
+// get request to display Hello on page on port 2000
 app.get("/",(req,res)=>{
-    res.send("Hello");
+    res.send("Hello Get Request");
 })
 
+//cerating middlewares 
+app.post("/", (req,res)=>{
+    res.send("This is the Post request ")
+})
+
+// app listening at port 2000
 app.listen(2000,()=>{
     console.log(`Running @ 2000`);
 })

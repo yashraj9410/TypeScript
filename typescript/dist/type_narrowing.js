3d27936153b873b3c2cea4b798dc19a403ede544
Yash <yashraj7011@gmail.com>
2023-03-23 11:41:50 +0530

    index-signatures

--- typescript/dist/type_narrowing.js
+++ typescript/dist/type_narrowing.js
@@ -1,1 +1,13 @@
 "use strict";
+let User = {
+    name: "yash",
+    age: 12
+};
+console.log(getUsername(User));
+function getUsername(a) {
+    var _a;
+    if (a.name) {
+        return (_a = a.name) === null || _a === void 0 ? void 0 : _a.toUpperCase();
+    }
+    return "User is not defined in the interface";
+}
--- typescript/src/type_narrowing.ts
+++ typescript/src/type_narrowing.ts
@@ -1,1 +1,23 @@
-// 
+// consider a interface 
+
+interface myObj {
+    name?:string,       // the value for key name may also be undefined 
+    age:number
+}
+
+let User:myObj = {
+    name:"yash",
+    age:12
+}
+
+console.log(getUsername(User));
+
+function getUsername(a:myObj){
+    // return a.name.toUpperCase();   // this line will create a error as the name property may be possibly undefined for some user 
+    if(a.name){
+        return a.name?.toUpperCase();          // here we have done narrowing to check if the name exists or not 
+    }
+
+    return "User is not defined in the interface";
+}
+    
